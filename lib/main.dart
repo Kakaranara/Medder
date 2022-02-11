e024cf7044a3773bd59bab2a865657ebaca8f0b0
Wahyu Koco <74239131+Kakaranara@users.noreply.github.com>
2022-02-11 15:08:51 +0700

    adding last online system

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -27,23 +29,47 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   bool isMed = false;
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  late Future<int> _lastTime;
+  late int time;
 
+  Future<void> handlepress() async{
+    final prefs = await _prefs;
+
+    setState(() {
+      _lastTime = prefs.setInt("gaga", time).then((bool succes) => time);
+      isMed = !isMed;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _lastTime = _prefs.then((SharedPreferences pref){
+      return pref.getInt("gaga") ?? 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    final minutes = DateTime.now().minute;
+    final hours = DateTime.now().hour;
+    final seconds = DateTime.now().second;
+    time = (hours * 3600) + (minutes * 60)  + seconds;
+
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         title: Center(
           child: Text(
-            "Medder",
+            " ~ Medicine Reminder ~ ",
             style: TextStyle(
               color: Colors.amber[200],
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
-        elevation: 0.0,
-
-
       ),
       body: Container(
         color: isMed ? Colors.green[200] : Colors.grey[200],
@@ -53,16 +79,22 @@ class _HomeState extends State<Home> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text(""),
+                Text("Your Last Take : $time"),
+                FutureBuilder<int>(
+                  future: _lastTime,
+                  builder: (BuildContext context, AsyncSnapshot<int> snapshot){
+                    return Text("Snapshot : ${snapshot.data}");
+                  },
+                ),
+                Text("Live time (second)  : $time"),
+                Text("Live Time : ${DateTime.now()}"),
                 const SizedBox(height: 20.0,),
                 ElevatedButton(
                   child: const Icon(
                     Icons.add_task,
                   ),
                   onPressed: (){
-                    setState(() {
-                      isMed = !isMed;
-                    });
+                    handlepress();
                   },
                   style: ElevatedButton.styleFrom(
                     shape: const CircleBorder(),
